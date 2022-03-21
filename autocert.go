501f04b46304fce56175eb7c1951a3cd3a8360c1
ahdekkers <alex@flowfabricator.com>
2022-03-21 10:52:18 +0000

    Added copyright

--- autocert.go
+++ autocert.go
@@ -1,4 +1,24 @@
 /*
+ * GNU Lesser General Public License
+ * Copyright (C) 2022 Spica Innovations S.R.L
+ * alex@flowfabricator.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
 Package autocert is an opinionated pki library for non-production use. This library can be used to set up a CA and sign
 certificates. There can only be 1 CA, and it must use a long-lived certificate (5+ years). All certificates use
 ed25519 public keys. CAs can be imported however they must be a root CA certificate.
